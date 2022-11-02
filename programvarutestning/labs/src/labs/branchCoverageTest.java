4726b63ce83cf82dc86eb31d457664afa2e44d63
Blenda Öhman <blenda.ohman@gmail.com>
2022-11-02 15:40:52 +0100

    new files

---
+++ programvarutestning/labs/src/labs/branchCoverageTest.java
@@ -0,0 +1,14 @@
+package labs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class branchCoverageTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ programvarutestning/labs/src/labs/predicateCoverageTest.java
@@ -0,0 +1,14 @@
+package labs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class predicateCoverageTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
